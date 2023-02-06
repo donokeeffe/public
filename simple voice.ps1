4be07c74e271315a8b2f148ea456cd901fee151d
donokeeffe <47123155+donokeeffe@users.noreply.github.com>
2023-02-06 14:54:29 +0000

    Delete simple voice.ps1

--- simple voice.ps1
+++
@@ -1,9 +0,0 @@
-﻿Add-Type -AssemblyName System.Speech
-$Speech = New-Object System.Speech.Synthesis.SpeechSynthesizer
-
-$Speech.GetInstalledVoices() | select -ExpandProperty VoiceInfo | select Name, Gender, Description
-
-$Speech.SelectVoice("Microsoft Hazel Desktop")
-
-$Message = 'This is awesome.  We now know how to use Powershell Text To Speech'
-$Speech.Speak($Message)
